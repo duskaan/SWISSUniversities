11057a12dd2b0a30f8e47751e24aed153768a68b
Duskaan <tim.vandijke@gmx.ch>
2018-01-13 22:41:42 +0100

    new pics and emails updated

--- view/contactEmail.php
+++ view/contactEmail.php
@@ -40,7 +40,7 @@ $this->message;
     </tr>
     </tbody>
 </table>
-<img src="https://swissstudyportal.herokuapp.com/assets/img/Logo_w_text_small.png" id="brand"
+<img src="https://swissstudyportal.herokuapp.com/assets/img/Logo.png" id="brand"
      style="padding-top:0px;margin-top:9px;margin-left:6px;size:100px"></a>
 </body>
 </html>
