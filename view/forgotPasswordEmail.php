f4088396baf9eefc4a3ca00d6ce38c580746b49e
Duskaan <tim.vandijke@gmx.ch>
2018-01-13 22:40:00 +0100

    new pics and emails updated

--- view/forgotPasswordEmail.php
+++ view/forgotPasswordEmail.php
@@ -37,7 +37,7 @@ $this->university
     </tr>
     </tbody>
 </table>
-<img src="https://swissstudyportal.herokuapp.com/assets/img/Logo_w_text_small.png" id="brand"
+<img src="https://swissstudyportal.herokuapp.com/assets/img/Logo.png" id="brand"
      style="padding-top:0px;margin-top:9px;margin-left:6px;size:100px"></a>
 </body>
 </html>
